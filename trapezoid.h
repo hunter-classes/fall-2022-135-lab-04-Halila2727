9887fd1b2f7b3a918837dd701da8829e54fec101
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-23 03:11:01 -0400

    added trapezoid() function

---
+++ trapezoid.h
@@ -0,0 +1,12 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Mike Zamansky
+    Assignment: Lab 04, Task F
+    This file serves as a means to let one file use the contents of another file.
+*/
+#pragma once
+#include <iostream>
+#include <string>
+
+std::string trapezoid(int width, int height);
