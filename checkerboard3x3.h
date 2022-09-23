32d21697971d5ca38f34acf4d05e5633db005ad6
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-23 04:01:39 -0400

    added checkerboard3x3() function

---
+++ checkerboard3x3.h
@@ -0,0 +1,12 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Mike Zamansky
+    Assignment: Lab 04, Task G
+    This file serves as a means to let one file use the contents of another file.
+*/
+#pragma once
+#include <iostream>
+#include <string>
+
+std::string checkerboard3x3(int width, int height);
