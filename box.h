02920dc91823cae63dd66370804e037a55723193
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-23 00:39:56 -0400

    added box() function

---
+++ box.h
@@ -0,0 +1,11 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Mike Zamansky
+    Assignment: Lab 04, Task A
+    This file serves as a means to let one file use the contents of another file.
+*/
+#include <iostream>
+#pragma once
+
+void box(int width, int height);
