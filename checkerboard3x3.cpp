b851def5f51d94803e6d02175d40544a3e0f9eec
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-23 04:27:32 -0400

    updated function to produce correct intended output

--- checkerboard3x3.cpp
+++ checkerboard3x3.cpp
@@ -12,39 +12,43 @@
 std::string checkerboard3x3(int width, int height)
 {
     std::string s = "";
-    std::string current_char = " ";
 
-    int three_lines = 3;
-    
-    for(int i=1; i<=height; i++)
+    std::string beginning_char = "*";
+    std::string current_char = "";
+
+    for(int i = 1; i <= height; i++)
     {
+        current_char = beginning_char;
         
-        for(int j=0; j < width; j++)
+        for(int j = 1; j <= width; j++)
         {
-            if(j%3 == 0 && current_char == "*")
-            {
-                current_char = " ";
-            }
-            else if(j%3 == 0 && current_char == " ")
-            {
-                current_char = "*";
-            }
             s += current_char;
-        }
 
+            if(j % 3 == 0)
+            {
+                if(current_char == "*")
+                {
+                    current_char = " ";
+                }
+                else
+                {
+                    current_char = "*";
+                }
+            }
+        }
         s += "\n";
+        
         if(i % 3 == 0)
         {
-            if(current_char == "*")
+            if(beginning_char == "*")
             {
-                current_char = " ";
+                beginning_char = " ";
             }
             else
             {
-                current_char = "*";
+                beginning_char = "*";
             }
         }
     }
-    
     return s;
 }
