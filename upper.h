4eade8e7f5a74e8c907b1d1f3461a887f8762455
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-23 02:45:05 -0400

    added upper() function

---
+++ upper.h
@@ -0,0 +1,12 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Mike Zamansky
+    Assignment: Lab 04, Task E
+    This file serves as a means to let one file use the contents of another file.
+*/
+#pragma once
+#include <iostream>
+#include <string>
+
+std::string upper(int length);
