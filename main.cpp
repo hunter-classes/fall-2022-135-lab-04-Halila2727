afc80da8f79e38e059ddde1d428f2284b8e2a819
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-23 00:40:34 -0400

    added and tested box() function

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Mike Zamansky
+    Assignment: Lab 04, All Tasks
+    This program will call the functions made in other .cpp files
+*/
+
+#include <iostream>
+#include "box.h"
+
+int main()
+{
+    std::cout << "\nWill return various boxes:\nWidth: 8\nHeight: 4";
+    box(8, 4);
+
+    return 0;
+}
