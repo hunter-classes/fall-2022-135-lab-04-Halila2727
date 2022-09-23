82cd80351e1b6cd14476409233555d4af82c8f3a
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-23 00:55:03 -0400

    Created more relevant tests for box() function

--- main.cpp
+++ main.cpp
@@ -7,12 +7,26 @@
 */
 
 #include <iostream>
+#include <string>
 #include "box.h"
 
 int main()
 {
-    std::cout << "\nWill return various boxes:\nWidth: 8\nHeight: 4";
-    box(8, 4);
+    std::cout << "\nTesting box() function:\n\nWidth: 8\nHeight: 4\n";
+    std::string result = box(8, 4);
+    std::cout << result << "\n";
+    std::cout << "\n------------------\n";
+    std::cout << "\nWidth: 14\nHeight: 0\n";
+    result = box(14, 0);
+    std::cout << result << "\n";
+    std::cout << "\n------------------\n";
+    std::cout << "\nWidth: 1\nHeight: 12\n";
+    result = box(1, 12);
+    std::cout << result << "\n";
+    
+    std::cout << "\nEnd of Task A\n------------------\n\n";
+
+
 
     return 0;
 }
